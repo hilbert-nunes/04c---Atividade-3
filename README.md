41d7923f377a3ac7b02505fc934a1bc98c0b8e23
Hilbert Nunes <hilbert.nunes.dev@gmail.com>
2024-07-24 20:42:13 -0300

    add pnf

---
+++ README.md
@@ -0,0 +1,1 @@
+![prove](https://github.com/user-attachments/assets/26c5b63a-6504-426e-882b-b415b0bb1fc7)
--- obj/Debug/net8.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+818340bdd5ba7601045ece791f8dd23e5cd52807")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
